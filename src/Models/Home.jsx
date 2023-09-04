a25bf83e003c2880c3d2e5b5761a350bbd8efb17
Manoj Gurung <gurungm109@gmail.com>
2023-09-04 13:59:24 -0500

    Some content for homepage

--- src/Models/Home.jsx
+++ src/Models/Home.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react'; 
+import {Link} from 'react-router-dom'; 
+import '../Home.css'; 
+import cafeteriaPhoto from '../images/cafeteria-photo.jpg'; 
 
-export default function Home() {
+
+function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home">
+      <header className="home-text-one">
+        <h1>Welcome to UNT Cafeteria @ Discovery Park </h1>
+        <p>Explore many mouth-watering dishes and more!!</p>    
+        <p>Hours of Operation</p>
+        <p>Monday-Thursday  7:30am - 9pm</p>
+        <p>Friday 7:30am - 8pm</p>
+        <p>Saturday 8:00am - 7pm</p>
+        <p>Sunday CLOSED!!!</p> 
+      </header>
+      
+      
+    </div>
+  ); 
   
 }
+
+export default Home; 

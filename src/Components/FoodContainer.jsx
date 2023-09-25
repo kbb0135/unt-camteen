a31b825d67c916df301bcf5ed162e843ec2c7cfb
Budhathoki, Karan <budhathokikaran135@gmail.com>
2023-09-24 21:22:04 -0500

    Rating Feature added for Review Site

--- src/Components/FoodContainer.jsx
+++ src/Components/FoodContainer.jsx
@@ -1,23 +1,60 @@
-import React from 'react'; 
-import "../style.css"; 
-import {ReviewRating} from "./ReviewRating"; 
+import React, { useState } from 'react'
+import '../style.css'
+import { ReviewRating } from "./ReviewRating";
+import { auth, db } from '../firebase.js'
+import { onAuthStateChanged } from 'firebase/auth';
+import { setDoc, doc } from 'firebase/firestore';
 
-function FoodContainer({foodImage, foodName, rating, totalReviews}) 
-{
+
+
+const FoodContainer = ({ item, newRating }) => {
+    const [rate, setRate] = useState(item.rating || 0)
+    console.log(1)
+    //  const handleRating = (newRating) => {
+    console.log(2)
+    try {
+        onAuthStateChanged(auth, (user) => {
+            console.log("here")
+            if (user) {
+                const uid = user.uid
+                console.log(uid)
+                // setDoc(doc(db, "Reviews", item.name), {
+                //     rating: 2
+                // })
+                setRate(newRating)
+                console.log("here")
+            }
+            else {
+                console.log("Something is wrong")
+            }
+        })
+    }
+    catch (error) {
+        console.log(error);
+        console.log("here")
+    }
+
+    // }
+
+
     return (
         <div className='food-container'>
-            <div className="food-image-container">
-                <img className='food-image' src={foodImage} alt="" />
-            </div>
-            <div className='food-info'>
-                <div className='food-name'>{foodName}</div>
-                <ReviewRating rating={rating} totalReviews={totalReviews} />
-                <div className='leave-review'>Leave a review.</div>
-            </div>
+            <div>
+                <div className="food-image-container">
+                    <img src={item.image} alt={item.name} />
+                </div>
+                <div className='food-info'>
+                    <span>{item.name}</span><br></br>
+                    <span>${item.price}</span><br></br>
+                    <span>{item.quantity}</span>
+                    <ReviewRating rating={rate} />
+                    <div className='leave-review'>Leave a review.</div>
+                </div>
 
-        </div>
-    ); 
+
+            </div>
+        </div >
+    )
 }
 
-
-export default FoodContainer; 
+export default FoodContainer
--- src/Models/Reviews.jsx
+++ src/Models/Reviews.jsx
@@ -1,128 +1,123 @@
-import React from "react";
-import chickenPizza from "../Assets/chicken-pizza.png";
+import React, { useState, useEffect } from "react";
 import FoodContainer from "../Components/FoodContainer";
-import GarlicButter from "../Assets/garlic-butter-chicken-wings.png";
-import Kebab from "../Assets/kebab.png";
-import Salmon from "../Assets/salmon.png";
-import PorkChop from "../Assets/pork-chops.png";
-import Steak from "../Assets/steak.png";
-import SalabBowl from "../Assets/vegan-salad-bowl.png";
-import ColdBrew from "../Assets/cold-brew.png";
-import Frappucciono from "../Assets/frappucino.png";
-import HotChocolate from "../Assets/hot-chocolate.jpeg";
-import Mocha from "../Assets/mocha.jpeg";
-import Americano from "../Assets/americano.png";
-import Coffee from "../Assets/coffee.png";
-import FrenchFries from "../Assets/french-fries.png";
-import ChickenWings from "../Assets/chicken-wings.png";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import "../review.css";
+import '../Admin/AdminMenu.css';
+import { getDocs, collection } from 'firebase/firestore'
+import { db} from '../firebase.js'
+
+
 
 export default function Reviews() {
+  const [menuItems, setMenuItems] = useState([]);
+  const getEntrees = async () => {
+    try {
+      const snapshot = await getDocs(collection(db, "Entrees"));
+      const entrees = snapshot.docs.map(doc => ({
+        id: doc.id,
+        name: doc.data().Name,
+        price: doc.data().Price,
+        image: doc.data().ImageURL
+      }));
+      return entrees;
+    } catch (error) {
+      alert(error);
+      return [];
+    }
+
+    
+
+  };
+  //getting side dishes from the collection
+  const getSide = async () => {
+    try {
+      const snapshot = await getDocs(collection(db, "Side"));
+      const sides = snapshot.docs.map(doc => ({
+        id: doc.id,
+        name: doc.data().Name,
+        price: doc.data().Price,
+        image: doc.data().ImageURL
+      }));
+      return sides;
+    } catch (error) {
+      alert(error);
+      return [];
+    }
+  };
+  //getting drinks from the collection
+  const getDrink = async () => {
+    try {
+      const snapshot = await getDocs(collection(db, "Drink"));
+      const drink = snapshot.docs.map(doc => ({
+        id: doc.id,
+        name: doc.data().Name,
+        price: doc.data().Price,
+        image: doc.data().ImageURL
+      }));
+      return drink;
+    } catch (error) {
+      alert(error);
+      return [];
+    }
+  };
+  //geting the desert from the collection
+  const getDesert = async () => {
+    try {
+      const snapshot = await getDocs(collection(db, "Desert"));
+      const desert = snapshot.docs.map(doc => ({
+        id: doc.id,
+        name: doc.data().Name,
+        price: doc.data().Price,
+        image: doc.data().ImageURL,
+        quantity: doc.data().quantity
+      }));
+      return desert;
+    } catch (error) {
+      alert(error);
+      return [];
+    }
+  };
+
+  //use effect feature to update the item as soon
+  // as the state changes
+  useEffect(() => {
+    //getting all menu items and setting them to state
+    const setData = async () => {
+      const desertData = await getDesert();
+      const entreesData = await getEntrees();
+      const sideData = await getSide();
+      const drinkData = await getDrink();
+      const mergeData =
+        [...entreesData,
+        ...desertData,
+        ...sideData,
+        ...drinkData
+        ];
+      if (mergeData.length > 0) {
+        setMenuItems(mergeData);
+        // console.log('fetched entree data', mergeData)
+      };
+    }
+
+    setData()
+  }, []);
+
   return (
     <div>
       <Header />
-      <div className="review-container">
-        <div className="review-row">
-          <FoodContainer
-            foodImage={chickenPizza}
-            foodName={"Chicken Pizza"}
-            rating={4.8}
-            totalReviews={132}
-          />
-          <FoodContainer
-            foodImage={GarlicButter}
-            foodName={"Garlic Butter Chicken"}
-            rating={4.5}
-            totalReviews={90}
-          />
-          <FoodContainer
-            foodImage={Kebab}
-            foodName={"Kebab"}
-            rating={5}
-            totalReviews={87}
-          />
-        </div>
-        <div className="review-row">
-          <FoodContainer
-            foodImage={SalabBowl}
-            foodName={"Salad Bowl"}
-            rating={4.2}
-            totalReviews={112}
-          />
-          <FoodContainer
-            foodName={"Salmon"}
-            foodImage={Salmon}
-            rating={3.7}
-            totalReviews={127}
-          />
-          <FoodContainer
-            foodName={"Chicken Wings"}
-            foodImage={ChickenWings}
-            rating={4.3}
-            totalReviews={66}
-          />
-        </div>
-        <div className="review-row">
-          <FoodContainer
-            foodImage={PorkChop}
-            foodName={"Pork Chop"}
-            rating={3.7}
-            totalReviews={143}
-          />
-          <FoodContainer
-            foodImage={Steak}
-            foodName={"Steak"}
-            rating={4.8}
-            totalReviews={89}
-          />
-          <FoodContainer
-            foodImage={FrenchFries}
-            foodName={"French Fries"}
-            rating={4.9}
-            totalReviews={93}
-          />
-        </div>
-        <div className="review-row">
-          <FoodContainer
-            foodImage={Americano}
-            foodName={"Americano"}
-            rating={4.7}
-            totalReviews={92}
-          />
-          <FoodContainer
-            foodImage={ColdBrew}
-            foodName={"Cold Brew"}
-            rating={4.6}
-            totalReviews={112}
-          />
-          <FoodContainer
-            foodImage={Frappucciono}
-            foodName={"Frappucino"}
-            rating={4.7}
-            totalReviews={102}
-          />
-        </div>
-        <div className="review-row">
-          <FoodContainer
-            foodImage={HotChocolate}
-            foodName={"Hot Chocolate"}
-            rating={4.7}
-            totalReviews={121}
-          />
-          <FoodContainer
-            foodImage={Mocha}
-            foodName={"Mocha"}
-            rating={4.3}
-            totalReviews={71}
-          />
-          <FoodContainer
-            foodImage={Coffee}
-            foodName={"Coffee"}
-            rating={3.5}
-            totalReviews={74}
-          />
+      <div className="menu-items">
+        <div className="review-container">
+          <div className="review-row">
+
+            {menuItems.map((item) => (
+              <FoodContainer
+                key={item.id}
+                item={item}
+              />
+            ))}
+
+          </div>
         </div>
       </div>
       <Footer />

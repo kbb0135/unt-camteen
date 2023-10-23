c25d72be8b949dac33367beff056eee7a0efc1dc
Budhathoki, Karan <budhathokikaran135@gmail.com>
2023-10-22 21:41:16 -0500

    Notify.jsx added along with the Cart Message feature

--- src/Models/Cart.jsx
+++ src/Models/Cart.jsx
@@ -1,28 +1,56 @@
 
-import React from 'react'
-import {useCart} from './CartContext'
+import React, { useState, useEffect } from 'react'
+import { useCart } from './CartContext'
 import "../style/Cart.css";
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import { Link } from 'react-router-dom';
 
 export default function Cart() {
-    const { cartItems, removeFromCart, addToCart } = useCart(); 
+  const { cartItems, removeFromCart, addToCart } = useCart();
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const newTotal = cartItems.reduce((acc, item) => {
+      return acc + (item.price * item.quantity);
+    }, 0)
+    setTotal(newTotal);
+  }, [cartItems])
 
   return (
     <div>
-    <Header />
+      <Header />
       <h2>Cart</h2>
-      <ul>
-        {cartItems.map((item) => (
-          <li key={item.id}>
-            <img src={item.image} className="img-cart"></img>-{item.name} - ${item.price}{' '}
-            <button onClick={() => addToCart(item)}>+</button>
-            <div>{item.quantity}</div>
-            <button onClick={() => removeFromCart(item)}>-</button>
-          </li>
-        ))}
-      </ul>
-      {/* <Footer /> */}
+      {cartItems.length === 0 ? (
+        <div>
+          <p>Your cart is empty! Add some items to get started.</p>
+          <Link to="/menu">
+            <button>Add Item</button>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <ul>
+            {cartItems.map((item) => (
+              <li key={item.id} className="cart-item">
+                <img src={item.image} className="img-cart"></img>-{item.name} - ${item.price}{' '}
+                <button onClick={() => addToCart(item)} className="plus-btn">+</button>
+                <div className="itemName-div">{item.quantity}</div>
+                <button onClick={() => removeFromCart(item.id)} className="minus-btn">-</button>
+              </li>
+            ))}
+          </ul>
+          <div>
+            <hr></hr>
+            <div className="food-total">
+              <div>Total Price: ${total.toFixed(2)}</div>
+            </div>
+            <button className="pay-btn">Pay {total.toFixed(2)}</button>
+          </div>
+
+          {/* <Footer /> */}
+        </div>
+      )}
     </div>
   );
 }

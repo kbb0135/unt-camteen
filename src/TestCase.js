eccc3ae532901901ba4805db91e58763b810e022
Budhathoki, Karan <budhathokikaran135@gmail.com>
2023-10-23 21:16:34 -0500

    Sending data to firebase from Cart with changes in Database System

--- src/TestCase.js
+++ src/TestCase.js
@@ -20,6 +20,7 @@ export default function TestCase (){
                         setRate(userData.rating)
                     }
                 }
+                
                 catch(error) {
                     console.log(error)
                 }

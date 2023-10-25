cadeb878dea1ea21b3c414efe52fc4ead5540433
Budhathoki, Karan <budhathokikaran135@gmail.com>
2023-10-24 23:03:30 -0700

    Remove feature added in backend database

--- src/Models/CartContext.jsx
+++ src/Models/CartContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth, db } from "../firebase.js"
-import { setDoc, doc, getDoc, updateDoc } from 'firebase/firestore';
+import { setDoc, doc, getDoc, updateDoc, deleteField } from 'firebase/firestore';
 
 
 const CartContext = createContext();
@@ -21,7 +21,7 @@ export function CartProvider({ children }) {
   });
 
   const addToCart = (item) => {
-    
+
     // Check if the item is already in the cart
     const existingItem = cartItems.find((cartItem) => cartItem.id === item.id);
 
@@ -32,7 +32,7 @@ export function CartProvider({ children }) {
           onAuthStateChanged(auth, async (user) => {
             const docRef = await doc(db, user.email, item.name);
             await updateDoc(docRef, {
-              quantity:cartItem.quantity+1
+              quantity: cartItem.quantity + 1
             })
           })
           return { ...cartItem, quantity: cartItem.quantity + 1 };
@@ -55,22 +55,22 @@ export function CartProvider({ children }) {
               await updateDoc(docRef, {
                 name: item.name,
                 price: item.price,
-                quantity:1  //for demo purpose
+                quantity: 1  //for demo purpose
               })
               console.log("test value")
-  
+
             }
             else {
               await setDoc(doc(db, user.email, item.name), {
                 name: item.name,
                 price: item.price,
-                quantity:1
+                quantity: 1
               })
             }
-  
+
           }
           catch {
-  
+
           }
         }
       })
@@ -78,7 +78,7 @@ export function CartProvider({ children }) {
       // If the item is not in the cart, add it with quantity 1
       setCartItems([...cartItems, { ...item, quantity: 1 }]);
     }
-    
+
 
   };
 
@@ -87,14 +87,63 @@ export function CartProvider({ children }) {
     const updatedCart = cartItems.reduce((accumulator, currentItem) => {
       if (currentItem.id === itemId) {
         if (currentItem.quantity > 1) {
+
           console.log("remove-", currentItem.quantity)
           accumulator.push({ ...currentItem, quantity: currentItem.quantity - 1 });
+          onAuthStateChanged(auth, async (user) => {
+            console.log("test1")
+            if (user) {
+              
+              try {
+                const docRef = await doc(db, user.email, currentItem.name)
+                const snapShot = await getDoc(docRef)
+                if (snapShot.exists()) {
+                  await updateDoc(docRef, {
+                    quantity: currentItem.quantity-1
+                  })
+                  
+
+                }
+
+              }
+              catch {
+
+              }
+            }
+          })
+
         }
+        else if(currentItem.quantity === 1) {
+          onAuthStateChanged(auth, async (user) => {
+            //deleting the data whenever the price quantity gets less than 1
+            const docRef = await doc(db, user.email, currentItem.name);
+            const snapShot = await getDoc(docRef);
+            if (snapShot.exists()) {
+              console.log("remove data from c")
+              console.log("email", user.email)
+              console.log("itemName", currentItem.name)
+              await updateDoc(docRef, {
+                name: deleteField(),
+                price: deleteField(),
+                quantity: deleteField()   
+              })
+              console.log("remove data c")
+            }
+            
+          })
+        }
+
       } else {
+          
+
+        
+
         accumulator.push(currentItem);
+        
       }
       return accumulator;
     }, []);
+
 
     setCartItems(updatedCart);
     localStorage.setItem("cartItems", JSON.stringify(updatedCart));
